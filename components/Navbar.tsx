5bba8a718541d262e3e03dd8b671e99e72fff5cf
EMAN <emansarwar3@gmail.com>
2023-10-22 14:48:02 +0600

    resume 2change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -84,7 +84,7 @@ const Navbar = () => {
               </motion.li>
             </Link>
           </ul>
-          <a href="/public/assets/MernEmanSarwarAlamResume.pdf" target="_blank">
+          <a href="/assets/MernEmanSarwarAlamResume.pdf" target="_blank">
             <motion.button
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
